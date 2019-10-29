d2c7edf302e3d1d350e997a80569294e340e2dc0
gyn <1170327450@qq.com>
2019-10-29 09:31:45 +0800

    no message

--- pages/Map/map.js
+++ pages/Map/map.js
@@ -1,28 +1,10 @@
-// pages/my/my.js
+// pages/Map/map.js
 Page({
-  /*事件触发*/
-  tapName: function (event) {
-    console.log(event)
-  },
-  click:function(){
-    console.log('点击了图片');
-   wx.navigateTo({
-     url: '../Map/map',
-   })
-    
 
-    /**
-     * push到另外一个页面 
-    */
-    // wx.navigateTo({
-    //   url: '../Push/push'
-    // })
-  },
   /**
    * 页面的初始数据
    */
   data: {
-    titleList: [{ title: "我的团队", img: "/images/555.jpg" }, { title: "我的项目", img: "/images/555.jpg" }, { title: "公司简介", img: "/images/555.jpg"}]
 
   },
 
@@ -30,55 +12,59 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+   wx.getLocation({
+     success: function(res) {
+       console.log(res)
+     },
+   })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
--- pages/classify/classify.js
+++ pages/classify/classify.js
@@ -1,19 +1,31 @@
 // pages/classify/classify.js
 Page({
   /**
+   * 点击方式
+  */
+  click:function(event){
+    console.log(event.currentTarget.dataset.id)
+  
+},
+
+  /**
    * 页面的初始数据
    */
   data: {
+    // tradeList:[]
      tradeList:[
-       { name: 'iOS', img: '/images/666.jpg', title: 'iOS作品集', goodsId: '1' }, { name: 'PHP', img: '/images/777.jpg', title: 'PHP作品集', goodsId: '2' }, { name: 'JAVA', img: '/images/888.jpg', title: 'JAVA作品集', goodsId: '3' }, { name: 'H5', img: "/images/999.jpg", title: 'H5作品集', goodsId: '4' }
+       { name: 'iOS', img: '/images/666.jpg', title: 'iOS作品集' }, { name: 'PHP', img: '/images/777.jpg', title: 'PHP作品集' }, { name: 'JAVA', img: '/images/888.jpg', title: 'JAVA作品集' }, { name: 'H5', img: "/images/999.jpg", title: 'H5作品集' }
      ]
   },
 
   /**
    * 生命周期函数--监听页面加载
+   * url写在这里
    */
   onLoad: function (options) {
-  
+  //  wx.request({
+  //    url: '',data:{'name':'小明'},method:'get',success:function(requestObject){this.data.tradeList=requestObject}
+  //  })
   },
 
   /**
@@ -45,21 +57,24 @@ Page({
   },
 
   /**
-   * 页面相关事件处理函数--监听用户下拉动作
+   * 页面相关事件处理函数--监听用户下拉动作 下拉刷新
    */
   onPullDownRefresh: function () {
   
   },
 
   /**
-   * 页面上拉触底事件的处理函数
+   * 页面上拉触底事件的处理函数    
    */
   onReachBottom: function () {
-  
+    //  wx.showLoading({
+    //    title: '加载中',
+    //  })
+
   },
 
   /**
-   * 用户点击右上角分享
+   * 用户点击右上角分享   这个是转发给朋友可以配置内容
    */
   onShareAppMessage: function () {
   
